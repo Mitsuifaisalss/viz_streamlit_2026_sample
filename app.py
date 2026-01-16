Fix scale parameter for normal distribution calculation

app.py
@@ -20,7 +20,7 @@ vars = st.sidebar.slider('正規分布の分散', min_value=0.1, max_value=20.0,
 
 # 標準正規分布の描画
 x_1 = np.linspace(-10, 10, 100)
-z = stats.norm.pdf(x_1, loc=mu, scale=vars)
+z = stats.norm.pdf(x_1, loc=mu, scale=np.sqrt(vars))
 
 fig_norm, ax1 = plt.subplots()
 ax1.plot(x_1, z, label='std_norm')